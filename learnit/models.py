e7f720c94c103a42522e2198450d88641e1fea25
RafayelGardishyan <rgardishyan@gmail.com>
2019-04-11 22:12:58 +0200

    fix

--- learnit/models.py
+++ learnit/models.py
@@ -12,6 +12,7 @@ class Question(models.Model):
 
 class Subject(models.Model):
     name = models.CharField(max_length=255)
+    name_dutch = models.CharField(max_length=255, default="UNKNOWN")
 
     def __str__(self):
         return self.name
